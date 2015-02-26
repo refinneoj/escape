ebba7f846e197e51529135be64680ff2bd14f28f
Eunice Dimacuta <eunice.dimacuta@bicol-u.edu.ph>
2015-02-26 11:40:38 -0800

    Update NewCursor.js

--- NewCursor.js
+++ NewCursor.js
@@ -1,11 +1,11 @@
 
-var yourCursor : Texture2D;	
-var cursorSizeX : int = 16;	
-var cursorSizeY : int = 16;	
+var yourCursor : Texture2D;	//Your cursor texture
+var cursorSizeX : int = 16;	//Your cursor size x
+var cursorSizeY : int = 16;	//Your cursor size y
 
 function Start()
 {
-		
+	Screen.showCursor = false;	
 }
 
 function OnGUI()
