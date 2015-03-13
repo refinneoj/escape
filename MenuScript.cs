7db5cebf784eef39eed5050927198468b24ec199
Eunice Dimacuta <eunice.dimacuta@bicol-u.edu.ph>
2015-03-13 00:56:35 -0700

    Main Menu buttons
    It contains two buttons, the "Play" and "Exit" buttons. When the player clicks the "Play" button  this will load our first level from our build settings. When the Player clicks the "Exit" button a popup Quit Menu will appear thus the Player will choose if he wants to quit the game with a "Yes" or "No". If he clicks "Yes" this will quit our game and exit to windows and if he clicks "No" we'll disable the quit menu, meaning it won't be visible anymore then enable the Play and Exit buttons again so they can be clicked.

--- MenuScript.cs
+++ MenuScript.cs
@@ -23,7 +23,7 @@ public class MenuScript : MonoBehaviour
 		
 	{
 		quitMenu.enabled = true; //enable the Quit menu when we click the Exit button
-		startText.enabled = false; //then disable the Play and Exit buttons so they cannot be clicked
+		startText.enabled = false; //then we disable the Play and Exit buttons so they cannot be clicked
 		exitText.enabled = false;
 		
 	}
